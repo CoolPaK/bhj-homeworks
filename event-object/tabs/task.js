58b983cd311b8c847377ed533b01f527398693c7
Oleg Valerevich <st-oleg58armia@mail.ru>
2024-07-26 14:11:09 +0300

    add task.js

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -4,15 +4,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
     tabs.forEach((tab, index) => {
         tab.addEventListener('click', () => {
+            const isActive = tab.classList.contains('tab_active');
+
             // Удаляем класс активной вкладки у всех вкладок
             tabs.forEach(tab => tab.classList.remove('tab_active'));
-            // Добавляем класс активной вкладке, на которую кликнули
-            tab.classList.add('tab_active');
+            // Добавляем класс активной вкладке, на которую кликнули, если она не активна
+            if (!isActive) {
+                tab.classList.add('tab_active');
+            }
 
             // Удаляем класс активного содержимого у всех вкладок
             tabContents.forEach(content => content.classList.remove('tab__content_active'));
-            // Добавляем класс активному содержимому, соответствующему вкладке
-            tabContents[index].classList.add('tab__content_active');
+            // Добавляем класс активному содержимому, соответствующему вкладке, если вкладка не активна
+            if (!isActive) {
+                tabContents[index].classList.add('tab__content_active');
+            }
+            // Иначе скрываем содержимое вкладки
+            else {
+                tabContents[index].classList.remove('tab__content_active');
+            }
         });
     });
 });

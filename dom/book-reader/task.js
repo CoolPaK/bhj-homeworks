16ddd67ad5cdbcc8a1ad3e8d1538f54942d6c5d8
Oleg Valerevich <st-oleg58armia@mail.ru>
2024-07-31 12:36:30 +0300

    add task.js

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,8 +1,7 @@
-const fontSizes = document.querySelectorAll('.font - size');
+const fontSizes = document.querySelectorAll('.font-size');
 const textColors = document.querySelectorAll('.color.text_color');
 const bgColors = document.querySelectorAll('.color.bg_color');
 const book = document.getElementById('book');
-
 
 // Функция для смены размера шрифта
 function changeFontSize(event) {
@@ -20,7 +19,6 @@ function changeFontSize(event) {
     } else {
         book.classList.remove('book_fs-big', 'book_fs-small');
     }
-
 }
 
 // Функция для смены цвета текста
@@ -32,7 +30,6 @@ function changeTextColor(event) {
 
     book.classList.remove('book_color-gray', 'book_color-whitesmoke', 'book_color-black');
     book.classList.add(`book_color-${event.target.dataset.textColor}`);
-
 }
 
 // Функция для смены цвета фона
@@ -44,7 +41,6 @@ function changeBgColor(event) {
 
     book.classList.remove('book_bg-gray', 'book_bg-black', 'book_bg-white');
     book.classList.add(`book_bg-${event.target.dataset.bgColor}`);
-
 }
 
 fontSizes.forEach(item => item.addEventListener('click', changeFontSize));

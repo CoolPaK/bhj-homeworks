2dada466be3030586eb92b99eff435dba326c274
Oleg Valerevich <st-oleg58armia@mail.ru>
2024-07-31 12:06:13 +0300

    add task.js

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,7 +1,7 @@
-// Получаем все элементы с классом reveal
+//Получаем все элементы с классом reveal
 const revealElements = document.querySelectorAll('.reveal');
 
-// Функция для проверки видимости элемента и добавления класса reveal_active
+//Функция для проверки видимости элемента и добавления класса reveal_active
 function checkVisibility() {
     revealElements.forEach(element => {
         const elementTop = element.getBoundingClientRect().top;
@@ -15,8 +15,8 @@ function checkVisibility() {
     });
 }
 
-// Слушаем событие прокрутки окна и вызываем функцию проверки видимости
+//Видим событие прокрутки окна и вызываем функцию проверки видимости
 window.addEventListener('scroll', checkVisibility);
 
-// Вызываем функцию при загрузке страницы, чтобы показать элементы, которые сразу видны
+//Вызываем функцию при загрузке страницы, чтобы показать элементы, которые сразу видны
 checkVisibility();
